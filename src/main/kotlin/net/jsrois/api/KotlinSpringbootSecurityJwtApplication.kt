3406c0d5eb627830385b0c57de67c340e13c277a
Javi <javi@jsrois.net>
2023-06-15 21:37:56 +0200

    README

---
+++ src/main/kotlin/net/jsrois/api/KotlinSpringbootSecurityJwtApplication.kt
@@ -0,0 +1,11 @@
+package net.jsrois.api
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KotlinSpringbootSecurityJwtApplication
+
+fun main(args: Array<String>) {
+    runApplication<KotlinSpringbootSecurityJwtApplication>(*args)
+}
